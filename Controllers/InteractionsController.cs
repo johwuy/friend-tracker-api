1151735c858eff1168d0232f45bba93ba925dc47
Johnny Wu <johwuy@gmail.com>
2025-08-12 05:56:07 +0000

    feat(interactions-controller): create interaction get dto

--- Controllers/InteractionsController.cs
+++ Controllers/InteractionsController.cs
@@ -8,6 +8,12 @@ namespace friend_tracker_api.Controllers;
 [Route("api/[controller]")]
 public class InteractionsController : ControllerBase
 {
+    public sealed class InteractionGetDto
+    {
+        public required Guid Id { get; set; }
+        public required string Content { get; set; }
+        public required DateTime Date { get; set; }
+    }
     private readonly ApplicationDbContext _context;
     private readonly ILogger<ContactsController> _logging;
     public InteractionsController(ApplicationDbContext context, ILogger<ContactsController> logging)
